77372e1809c15cafd728c37345a12031b9d0a18e
Unknown <cedenoparedes04@gmail.com>
2019-03-01 16:01:42 -0400

    adding model to app api

--- success_institute/api/models.py
+++ success_institute/api/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User, AbstractUser
 from django.db import models
 
+
 # Create your models here.
+
+class student(models.Model):
+    user = models.ForeignKey(User, related_name='user_studente_id', on_delete=models.DO_NOTHING)
+
+
+
