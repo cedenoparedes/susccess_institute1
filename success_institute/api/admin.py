577d88351420df4f25d279eed83faf29d03e66b5
Unknown <cedenoparedes04@gmail.com>
2019-03-07 00:36:31 -0400

    adding models and login view

--- success_institute/api/admin.py
+++ success_institute/api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from api import models
 # Register your models here.
+
+admin.site.register(models.student)
+admin.site.register(models.group)
+admin.site.register(models.student_group)
--- success_institute/api/models.py
+++ success_institute/api/models.py
@@ -1,11 +1,24 @@
 from django.contrib.auth.models import User, AbstractUser
 from django.db import models
-
+from django.utils.translation import ugettext_lazy as _
 
 # Create your models here.
 
 class student(models.Model):
     user = models.ForeignKey(User, related_name='user_studente_id', on_delete=models.DO_NOTHING)
+    career = models.CharField( max_length=128, blank=True, null=True)
+
+
+class group(models.Model):
+    name = models.CharField( max_length=128, blank=True, null=True)
+
+
+
+class student_group(models.Model):
+    student = models.ForeignKey(student,related_name ='student_group_id', on_delete=models.DO_NOTHING)
+    group_id = models.ForeignKey(group, related_name='student_group_id', on_delete=models.DO_NOTHING)
+    active = models.BooleanField(default=True)
+
 
 
 
